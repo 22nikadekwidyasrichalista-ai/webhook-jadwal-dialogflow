b1a73bbebf198446307177926282baa471866142
22nikadekwidyasrichalista-ai <22.ni.kadek.widya.sri.chalista@gmail.com>
2026-02-26 08:25:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.post("/webhook", (req,res)=>{
   const hari = req.body.queryResult.parameters.hari;
 
   const jadwal = {
-    senin: "08.10-09.30 Bahasa Bali , 08.50 PPKN, 09.30 Bahasa Bali",
-    selasa: "07.30 Inggris, 08.50 Sejarah",
-    rabu: "07.30 Agama Hindu",
-    kamis: "07.30 Informatika",
-    jumat: "07.30 Olahraga"
+    senin: "08.10-09.30 Bahasa Bali, 09.30-14.20 IPAS, 14.20-15.40 Bahasa Indonesia ",
+    selasa: "07.30-09.30 PJOK, 09.30-14.20 DDA, 14.20-15.40 Matematika ",
+    rabu: "07.30-08.50 PPKn, 08.50-10.10 Matematika, 10.40-12.00 Seni budaya, 12.00-13.20 Sejarah, 13.40-14.20 BK, 14.20-15.40 Bahasa indonesia ",
+    kamis: "07.30-08.50 KKA, 08.50-13.20 DDA, 13.40-15.40 Agama Hindu ",
+    jumat: "08.00-10.00 Informatika, 10.40-12.40 Bahasa Ingris ",
   };
 
   const reply = jadwal[hari] || "Jadwal tidak ditemukan";
